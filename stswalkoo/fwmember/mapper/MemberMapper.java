d6b6ea271b1b96a9299a94a9778f72059c64f9f0
nahee JO <105144666+Jonaheecode@users.noreply.github.com>
2023-11-01 11:46:18 +0900

    Delete stswalkoo directory

--- stswalkoo/fwmember/mapper/MemberMapper.java
+++
@@ -1,8 +0,0 @@
-package com.stswalkoo.fwmember.mapper;
-
-import com.stswalkoo.fwmember.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberMapper extends JpaRepository<Member, Long> {
-    Member findByUserEmail(String userEmail);
-}
